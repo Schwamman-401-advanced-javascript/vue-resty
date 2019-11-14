cff212f76b064acb86e92f4d3b20c49d4da21210
schwamman <schwamman@msn.com>
2019-11-14 15:31:11 -0600

    Implemented basic todo list

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>TODOS:</h1>
+    <input type="text" class="todo-input" placeholder="Add new item" v-on:keyup.enter="addTodo">
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <label>
+          <input type="checkbox" class="done-checkbox" v-model="todo.done">
+          <span>&nbsp;</span>
+          <del v-if="todo.done">{{ todo.text }}</del>
+          <span v-else>{{ todo.text }}</span>
+          <div class="space"></div>
+          <button class="delete-btn" v-on:click="removeTodo(todo.id)">X</button>
+        </label>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: () => {
+    return { todos: [{text: 'todo 1', done: false, id: Date.now()}]
+           }
+  },
+  methods: {
+    addTodo({target}){
+      this.todos.push({text: target.value, done: false, id: Date.now()})
+      target.value = ''
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
   }
 }
 </script>
