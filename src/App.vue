5204a74f725dbd6154d3bc0a38bb76b8afa519bc
schwamman <schwamman@msn.com>
2019-11-14 17:01:38 -0600

    Implemented connected todo list

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
   <div id="app">
-    <h1>TODOS:</h1>
-    <input type="text" class="todo-input" placeholder="Add new item" v-on:keyup.enter="addTodo">
+    <h2>There are {{numItems}} items to complete!</h2>
+    <form @submit.prevent="addTodo">
+      <label>
+        <span>To Do Item</span>
+        <input
+          type="text"
+          placeholder="Add To Do List Item"
+          v-model="item.text"
+        />
+      </label>
+      <label>
+        <span>Difficulty Rating</span>
+        <input type="range" min="1" max="5" v-model="item.difficulty" defaultValue="3" />
+      </label>
+      <label>
+        <span>Assigned To</span>
+        <input type="text" v-model="item.assignee" placeholder="Assigned To" />
+      </label>
+      <label>
+        <span>Due</span>
+        <input type="date" v-model="item.due" />
+      </label>
+      <button type="submit">Add Item</button>
+    </form>
     <ul>
-      <li v-for="todo in todos" :key="todo.id">
+      <li v-for="todo in todoList" :key="todo.id">
         <label>
-          <input type="checkbox" class="done-checkbox" v-model="todo.done">
+          <input type="checkbox" class="done-checkbox" v-on:click="setItemToDone(todo._id)">
           <span>&nbsp;</span>
-          <del v-if="todo.done">{{ todo.text }}</del>
+          <del v-if="todo.complete">{{ todo.text }}</del>
           <span v-else>{{ todo.text }}</span>
           <div class="space"></div>
-          <button class="delete-btn" v-on:click="removeTodo(todo.id)">X</button>
+          <button class="delete-btn" v-on:click="removeItem(todo._id)">X</button>
         </label>
       </li>
     </ul>
@@ -18,21 +40,61 @@
 </template>
 
 <script>
+const superagent = require('superagent');
 
 export default {
   name: 'app',
   data: () => {
-    return { todos: [{text: 'todo 1', done: false, id: Date.now()}]
+    return { todoList: [],
+             numItems: 0,
+             item: { 
+                      text: undefined,
+                      due: undefined,
+                      difficulty: 3,
+                      complete: false,
+                      assignee: undefined
+                   }
            }
   },
   methods: {
     addTodo({target}){
-      this.todos.push({text: target.value, done: false, id: Date.now()})
-      target.value = ''
+      superagent
+        .post('https://api-js401.herokuapp.com/api/v1/todo/')
+        .send(this.item)
+          .then(() => { superagent.get('https://api-js401.herokuapp.com/api/v1/todo/')
+                          .then(response => {this.todoList = response.body.results})
+                      });
+      target.value = '';
+      
     },
-    removeTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
+    removeItem(id) {
+      superagent
+        .delete(`https://api-js401.herokuapp.com/api/v1/todo/${id}`)
+          .then(() => { superagent.get('https://api-js401.herokuapp.com/api/v1/todo/')
+                          .then(response => {this.todoList = response.body.results})
+                      });
+    },
+    setItemToDone(id) {
+      let updatedItem = this.todoList.find(item => item._id === id);
+      if(updatedItem._id) {
+        updatedItem = {...updatedItem, complete: !updatedItem.complete};
+        superagent
+          .put(`https://api-js401.herokuapp.com/api/v1/todo/${id}`)
+          .send(updatedItem)
+            .then(() => { superagent.get('https://api-js401.herokuapp.com/api/v1/todo/')
+                          .then(response => {this.todoList = response.body.results})
+                      });
+      }
     }
+  },
+  mounted() {
+    superagent.get('https://api-js401.herokuapp.com/api/v1/todo/')
+      .then(response => {this.todoList = response.body.results})
+  },
+  updated() {
+    superagent.get('https://api-js401.herokuapp.com/api/v1/todo/')
+      .then(response => {this.todoList = response.body.results})
+      .then(() => {this.numItems = this.todoList.filter( item => !item.complete ).length})
   }
 }
 </script>
